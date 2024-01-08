5960b1f702d7c16bb3627e81427faa01f844ca5e
Mongkol-Kmutnb <139145399+Mongkol-Kmutnb@users.noreply.github.com>
2024-01-08 11:38:35 +0700

    class component
    day month years

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Header } from './func-components';
 import { Content } from './func-components';
 import { Footer } from './func-components';
+import Calendar from './class-components';
 function App() {
   return (
     <>
       <Header />
+      <p><center><Calendar /></center></p>
       <Content />
       <Footer />
     </>
