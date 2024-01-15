a9f146941c305a85f47d1649e3afda4fa8e53908
Mongkol-Kmutnb <139145399+Mongkol-Kmutnb@users.noreply.github.com>
2024-01-15 10:53:06 +0700

    calculator in function component
    calculator in function component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 import Button from './class-components';
+import { Calculator, Calculator2 } from './calculator';
+
 function App() {
   return (
     <>
       <p><center><Button /></center></p>
+      <Calculator2 />
     </>
   )
 }
