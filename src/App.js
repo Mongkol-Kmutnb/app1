98108002ccff176174d858561adc17701a3c3258
Mongkol-Kmutnb <139145399+Mongkol-Kmutnb@users.noreply.github.com>
2024-01-15 10:08:42 +0700

    arrow function inEvent Hander
    arrow function in Event Hander

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react'
-import { Header } from './func-components';
-import { Content } from './func-components';
-import { Footer } from './func-components';
-import Calendar from './class-components';
+
+import Button from './class-components';
 function App() {
   return (
     <>
-      <Header />
-      <p><center><Calendar /></center></p>
-      <Content />
-      <Footer />
+      <p><center><Button /></center></p>
     </>
   )
 }
