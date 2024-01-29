f53d6ce60fd14e452be02d76718eff0377a0da9a
Mongkol-Kmutnb <139145399+Mongkol-Kmutnb@users.noreply.github.com>
2024-01-29 10:31:04 +0700

    context-content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React from 'react'
-import MessageBox from './state-func'
-export default function App() {
-
-  return < MessageBox />
+import React from "react";
+import { userContext } from "./context";
+import Content from "./context-content";
+function App() {
+  return (
+    <>
+      <userContext.Provider value={"Tom Jerry"}>
+        <Content />
+      </userContext.Provider>
+    </>
+  );
 }
+
+export default App;
